68cebc2f54981187b47f15e298397b27ed88bc4f
SEUNGHUN,SHIN <42256738+softho0n@users.noreply.github.com>
2019-07-15 12:52:04 +0900

    Add files via upload

--- SSD_Viewer/src/skku/edu/SSDGUIFORM.java
+++ SSD_Viewer/src/skku/edu/SSDGUIFORM.java
@@ -1,19 +1,35 @@
 package skku.edu;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+import java.util.Vector;
 
 public class SSDGUIFORM extends JFrame
 {
     private JButton Mybutton;
-    private JTextArea TextArea;
     private JPanel rootPanel;
+    private JList list1;
+    private JScrollPane scrPane;
+    private Vector fileInfo;
+    private Random random;
+    private String[] fileName = {"1.txt","2.txt","3.txt","4.txt","5.txt"};
 
     public SSDGUIFORM()
     {
+        random = new Random();
+        fileInfo = new Vector();
+
+        for (int i = 0; i < 1000; i++) {
+            int random_varible = random.nextInt(5);
+            fileInfo.addElement(file[random_varible]);
+        }
+
+        list1.setListData(fileInfo);
+        list1.setSelectedIndex(0);
+
         add(rootPanel);
         setTitle("This is SSD Viewer");
         setSize(400,500);
-
-
     }
 }
