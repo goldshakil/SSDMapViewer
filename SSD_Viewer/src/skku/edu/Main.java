75930d78dab59a7a5e353759612bf46fec1c0831
pleiadex <rudobco0825@gmail.com>
2019-07-16 14:38:45 +0900

    LBA PBA table

--- SSD_Viewer/src/skku/edu/Main.java
+++ SSD_Viewer/src/skku/edu/Main.java
@@ -9,10 +9,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
 	// write your code here
 
-        SSDGUIFORM myGUIFORM=new SSDGUIFORM();
-        myGUIFORM.setVisible(true);
+        //SSDGUIFORM myGUIFORM=new SSDGUIFORM();
+        //myGUIFORM.setVisible(true);
 
         //MappingTable myMapping = new MappingTable();
         //myMapping.setVisible(true);
+
+        //PBAtable myPBA = new PBAtable("file1");
+        //myPBA.setVisible(true);
+
     }
 }
