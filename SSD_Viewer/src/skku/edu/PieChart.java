9c0d84a1a2141ba247129144165a357f0dd64629
Muhammad Shakeel Zuhaib (Dahab) <48248272+goldshakil@users.noreply.github.com>
2019-07-17 20:33:33 +0900

    added the segmentation code

--- SSD_Viewer/src/skku/edu/PieChart.java
+++ SSD_Viewer/src/skku/edu/PieChart.java
@@ -1,9 +1,11 @@
+//version 1.0.0
 package skku.edu;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PiePlot;
 import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.data.general.PieDataset;
 
@@ -11,7 +13,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 
 public class PieChart extends JFrame {
 
@@ -19,6 +25,11 @@ public class PieChart extends JFrame {
     private JLabel label;
     private JButton backBtn;
     private JPanel graphPanel;
+    Vector<String> LBA_vec = new Vector<String>();
+    Vector<String> PBA_vec = new Vector<String>();
+    Vector<Integer>PBA_vec_converted=new Vector<Integer>();
+    //for storing ranges
+    //<Integer, Integer> ranges = new HashMap<>();
 
     public PieChart(String file_name) throws IOException {
 
@@ -28,14 +39,101 @@ public class PieChart extends JFrame {
        setContentPane(panel1);
        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(1000,700);
-        setTitle("Pie Chart");
+        setTitle("Internal fragmentation");
         setVisible(true);
 
-        /*Chart ..*/
+        /*Reading */
+        File file = new File("C:\\Users\\Dahab Shakeel\\Desktop\\SSD_Viewer\\src\\skku\\edu\\filenames.txt"); //file path .. please change according to your input file
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        //reading from a buffer into a string
+        String st;
+        int ack = 0;
+        while ((st = br.readLine()) != null)
+        {
+            if(st.equals("END")) ack = 0;
+
+            if(ack == 1){
+
+                //TODO: LBA_vec, PBA_vec push back
+
+
+                LBA_vec.addElement(st.split("\\s")[0]);
+                PBA_vec.addElement(st.split("\\s")[2]);
+            }
+            if(st.equals(file_name+":")) ack = 1;
+        }
+
+        /*Convert the String Vector to Integer*/
+        int size=PBA_vec.size();
+       for(int i=0;i<size;i++)
+       {
+           PBA_vec_converted.addElement(Integer.parseInt(PBA_vec.get(i)));
+       }
+
+
+       /*Sorting and finding the maximum (element at the last index(size-1))*/
+        Collections.sort(PBA_vec_converted);
+
+
+        /*find number of ranges+seperate ranges*/
+        int[][] ranges = new int[size][2];//at max there will be "size" number of elements
+        int array_counter=0;//for looping through the array
+
+        int groups=0;//number of groups
+        int range_numbers=0;//number of elements within a group
+        for(int i=0;i<size;i++)
+        {
+            range_numbers++;
+            if(i==size-1)
+            {
+                System.out.println(PBA_vec_converted.get(i));
+                System.out.println(range_numbers);
+                ranges[array_counter][0]=PBA_vec_converted.get(i)-range_numbers+1; //start
+                ranges[array_counter][1]=PBA_vec_converted.get(i); //end
+
+                break;
+            }
+
+          else if(PBA_vec_converted.get(i)+1!=PBA_vec_converted.get(i+1))
+          {
+              System.out.println(PBA_vec_converted.get(i));
+              System.out.println(range_numbers);
+              ranges[array_counter][0]=PBA_vec_converted.get(i)-range_numbers+1; //start
+              ranges[array_counter][1]=PBA_vec_converted.get(i); //end
+              range_numbers=0;
+              groups++;
+              array_counter++;
+          }
+        }
+        groups++;//number of groups=number of separations+1
+
+        for(int j=0;j<groups;j++)
+        {
+            System.out.printf("%d \t %d\n",ranges[j][0],ranges[j][1]);
+        }
+        //smallest value in the ranges is ranges[0][0] -> subtract from all
+        /*add values to the chart*/
         DefaultPieDataset dataSet=new DefaultPieDataset();
-        dataSet.setValue("a",20.0);
-        dataSet.setValue("b",2.5);
-        JFreeChart pieChart= ChartFactory.createPieChart("PBA",dataSet);
+        for(int j=0;j<groups;j++)
+        {
+            dataSet.setValue(Integer.toString(ranges[j][1]),ranges[j][1]-ranges[j][0]+1);//adding the range segment
+            //adding the gap segment
+            dataSet.setValue("gap"+Integer.toString(j),ranges[j+1][0]-ranges[j][1]+1);
+
+
+        }
+        /*Color the chart to black and red*/
+        JFreeChart pieChart= ChartFactory.createPieChart("Internal PBA fragmentation for "+file_name,dataSet);
+
+        PiePlot plot = (PiePlot) pieChart.getPlot();
+        for(int j=0;j<groups;j++)
+        {
+            plot.setSectionPaint(Integer.toString(ranges[j][1]), Color.RED);
+            //adding the gap segment
+            plot.setSectionPaint("gap"+Integer.toString(j), Color.BLACK);
+        }
+
 
 
         ChartFrame chartFrame=new ChartFrame("PBA",pieChart,true);
