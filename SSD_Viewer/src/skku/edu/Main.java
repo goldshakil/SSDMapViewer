83de011a9580d672539009adb65105d846f2957f
pleiadex <rudobco0825@gmail.com>
2019-07-18 10:52:59 +0900

    main function edit ver

--- SSD_Viewer/src/skku/edu/Main.java
+++ SSD_Viewer/src/skku/edu/Main.java
@@ -8,8 +8,8 @@ public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 	// write your code here
 
-        //SSDGUIFORM myGUIFORM=new SSDGUIFORM();
-        //myGUIFORM.setVisible(true);
+        SSDGUIFORM myGUIFORM=new SSDGUIFORM();
+        myGUIFORM.setVisible(true);
 
         //MappingTable myMapping = new MappingTable();
         //myMapping.setVisible(true);
@@ -17,8 +17,8 @@ public class Main {
         //PBAtable myPBA = new PBAtable("file1");
         //myPBA.setVisible(true);
 
-        PBA_distribution mydistribution = new PBA_distribution("file1");
-        mydistribution.setVisible(true);
+        //PBA_distribution mydistribution = new PBA_distribution("file1");
+        //mydistribution.setVisible(true);
 
     }
 }
