bf80239f0b8a52e7e87984a60e3abf6312b35cc5
Pleiadex <rudobco0825@gmail.com>
2019-07-31 19:12:41 +0900

    PBA table is chagned into jtable

--- SSD_Viewer/src/skku/edu/PBAtable.java
+++ SSD_Viewer/src/skku/edu/PBAtable.java
@@ -1,6 +1,8 @@
 package skku.edu;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,12 +16,12 @@ import java.util.Vector;
 public class PBAtable extends JFrame{
     private JButton back_button;
     private JPanel rootPanel;
-    private JList LBA_list;
     private JLabel label;
-    private JList PBA_list;
-    private JScrollPane scrPane;
-    Vector<String> LBA_vec = new Vector<String>();
-    Vector<String> PBA_vec = new Vector<String>();
+    private JTable mapping_table;
+    private JScrollPane jscrollpane;
+    Vector<String> userColumn = new Vector<>();
+    DefaultTableModel model;
+
     String file_name="";
 
     public PBAtable(String Path, String file_name) throws IOException, ClassNotFoundException {
@@ -27,6 +29,9 @@ public class PBAtable extends JFrame{
         System.out.println("pba file name: "+file_name);
 
         label.setText(" "+file_name);
+        userColumn.addElement("LBA");
+        userColumn.addElement("PBA");
+        model = new DefaultTableModel(userColumn, 0);
 
         /*File I/O*/
         File file = new File(Path); //file path .. please change according to your input file
@@ -43,33 +48,28 @@ public class PBAtable extends JFrame{
             }
             if(ack == 2){
                 if(!st.split("\t")[1].equals("4294967295")) {
+                    Vector<String> userRow = new Vector<>();
+                    System.out.println(">"+st.split("\t")[0]);
+                    System.out.println(">"+st.split("\t")[1]);
 
-                    LBA_vec.addElement(st.split("\t")[0]);
-                    PBA_vec.addElement(st.split("\t")[1]);
+                    userRow.addElement(st.split("\t")[0]);
+                    userRow.addElement(st.split("\t")[1]);
+                    model.addRow(userRow);
                 }
             }
             if(st.equals(file_name)&&ack==1) ack = 2;
         }
 
-
-
-        /*Setting the data to our list*/
-        LBA_list.setListData(LBA_vec);
-        LBA_list.setSelectedIndex(0);
-        PBA_list.setListData(PBA_vec);
-        LBA_list.clearSelection();
-        PBA_list.clearSelection();
+        mapping_table = new JTable(model);
+        jscrollpane.setViewportView(mapping_table);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
 
         add(rootPanel);
         setTitle("LBA PBA Table");
         setSize(400,500);
 
-
         back_button.setPreferredSize(new Dimension(10,10));
-
 
         back_button.addActionListener(new ActionListener() {
             @Override
